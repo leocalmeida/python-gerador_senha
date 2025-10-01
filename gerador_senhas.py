d398b2cd12c596cecde9f1716fa120e2670dfb2a
almleo <58697952+almleo@users.noreply.github.com>
2025-10-01 07:12:27 -0300

    finalizada a logica para a geração das senhas

--- gerador_senhas.py
+++ gerador_senhas.py
@@ -1,4 +1,5 @@
 import string
+import random
 
 letters = string.ascii_letters
 numbers = string.digits
@@ -11,8 +12,26 @@ menu = '''Gerador de Senhas Fortes
 '''
 
 def generate_passkey():
-    length = int(input("Quantos caracteres deve conter a senha, 8, 10 ou 12?\n")) 
-        
+    lengths = [8,10,12]
+    length = 0
+    passkey = ''
+    deck = [letters, numbers, special]
+    
+    while (length not in lengths):
+        length = int(input("Quantos caracteres deve conter a senha, 8, 10 ou 12?\n")) 
+    
+    while len(passkey) < length:
+        random.shuffle(deck)
+        x = random.randrange(0,3)
+        y = random.randrange(0,len(deck[x]))
+        passkey += deck[x][y]
+    print("-------------------------")
+    print('Gerando a senhas...')
+    print("-------------------------")
+    
+    return passkey + '\n'
+
+def txt_export(passkey):
     pass
 
 
@@ -20,13 +39,21 @@ def main():
     
     option = 9
     while option != 0:
+        passkey = ''
+        
         print(menu)
         option = int(input("Digite a opção desejada:\n"))
+        
         if option == 1:
-            # generate_passkey()
-            pass
+            passkey = generate_passkey()
+            print(passkey)
+            
         elif option == 2:
-            pass
+            txt_export(passkey)
+            
+        elif option == 0:
+            print("Saindo.....")
+            break
         else:
             print("invalid option")
     
