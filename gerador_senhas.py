6e69c1f97540883b88ab5f78c51259967edd81a0
almleo <58697952+almleo@users.noreply.github.com>
2025-10-03 06:57:08 -0300

    adicionado docString e try excepction para tratativa das entradas

--- gerador_senhas.py
+++ gerador_senhas.py
@@ -1,6 +1,8 @@
+
 import string
 import random
 import os
+
 
 letters = string.ascii_letters
 numbers = string.digits
@@ -13,13 +15,22 @@ menu = '''Gerador de Senhas Fortes
 '''
 
 def generate_passkey():
+    """
+    Retorna uma senha segura de 8, 10 ou 12 caracteres
+
+    Returns:
+        string: Senha gerada a partir do processamento randomizado de dados
+    """
     lengths = [8,10,12]
     length = 0
     passkey = ''
     deck = [letters, numbers, special]
     
     while (length not in lengths):
-        length = int(input("Quantos caracteres deve conter a senha, 8, 10 ou 12?\n")) 
+        try:
+            length = int(input("Quantos caracteres deve conter a senha, 8, 10 ou 12?\n")) 
+        except ValueError:
+            print("Entrada inválida! Digite um numero.\n")
     
     while len(passkey) < length:
         random.shuffle(deck)
@@ -33,33 +44,48 @@ def generate_passkey():
     return passkey + '\n'
 
 def txt_export(passkey):
+    """Exportação de senha em arquivo texto
+
+    Args:
+        passkey string: senha gerada pelo método generate_passkey
+
+    Returns:
+        string: caminho absoluto do arquivo gerado
+    """
     file = "senha_exportada.txt"
         
     with open(file, 'w', encoding="utf-8") as arquivo:
         arquivo.write(passkey)
     
-    absolute_path = os.path.abspath(file)
+    return os.path.abspath(file)
     
-    print(f"Senha salva no arquivo!{absolute_path}")
+    
     
 
 
 def main():
-    
+    """Método main
+    Ele controla o fluxo de execução do programa
+    """
     option = 9
     passkey = ''
     
     while option != 0:
         
         print(menu)
-        option = int(input("Digite a opção desejada:\n"))
+        try:
+            option = int(input("Digite a opção desejada:\n"))
+        except ValueError:
+            print("Entrada inválida! Digite um numero.\n")
+            continue
         
         if option == 1:
             passkey = generate_passkey()
             print(passkey)
             
         elif option == 2:
-            txt_export(passkey)
+            absolute_path = txt_export(passkey)
+            print(f"Senha salva no arquivo!{absolute_path}")
             
         elif option == 0:
             print("Saindo.....")
